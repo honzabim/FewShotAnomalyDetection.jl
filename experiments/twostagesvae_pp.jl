1fe1754da36d113028cb7fe0e1b397987ebb48a5
honzabim <honzabim@gmail.com>
2019-03-29 09:35:40 +0100

    Fixed postprocessing of the two stage svae

--- experiments/twostagesvae_pp.jl
+++ experiments/twostagesvae_pp.jl
@@ -23,3 +23,4 @@ for d in unique(results[:dataset])
     mean_auc_pz = mean(ddf[:auc_pz])
     push!(aggres, DataFrame(dataset = d, auc_pxv = mean_auc_pxv, auc_pz = mean_auc_pz))
 end
+aggres = vcat(aggres...)
