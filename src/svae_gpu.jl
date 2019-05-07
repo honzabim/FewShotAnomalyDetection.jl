cd2a5b6731643924390fb897009d6f7abcc7fbcd
Jan Bim <bimjan@bayes.felk.cvut.cz>
2019-05-07 14:47:50 +0200

    finished gpu samplers for gamma and beta

--- src/svae_gpu.jl
+++ src/svae_gpu.jl
@@ -1,6 +1,8 @@
 # normalizecolumns(m::CuArray{T, 2}) where {T} = m ./ sqrt.(sum(m .^ 2, dims = 1)) .+ eps(T)
 # normalizecolumns(m::TrackedArray{T, 2, CuArray{T, 2}}) where {T} = m ./ sqrt.(sum(m .^ 2, dims = 1)) .+ eps(T)
 
+using CUDAnative
+
 GPUArray{T, N} = Union{CuArray{T, N}, TrackedArray{T, N, CuArray{T, N}}}
 
 samplehsuniform_gpu(size...) = samplehsuniform_gpu(Float32, size...)
@@ -17,16 +19,15 @@ function samplez(m::SVAE, μz::GPUArray{T, 2}, κz::GPUArray{T, 2}) where {T}
 	return z
 end
 
-rand_gamma_gpu(k::Number, θ::Number, size::Int...) = rand_gamma_gpu(Float32, k, θ, size...)
+
 rand_gamma_cpu(k::Number, θ::Number, size::Int...) = rand_gamma_cpu(Float32, k, θ, size...)
-rand_gamma_gpu(T::Type, k, θ, size...) = rand_gamma(T::Type, k, θ, cufill, cuzeros, size...)
-rand_gamma_cpu(T::Type, k, θ, size...) = rand_gamma(T::Type, k, θ, fill, zeros, size...)
+rand_gamma_cpu(T::Type, k::Number, θ::Number, size::Int...) = rand_gamma(T::Type, k, θ, fill, zeros, size...)
 function rand_gamma(T::Type, k, θ, fillfun, zerosfun, size...)
     k = T(k)
     θ = T(θ)
     if k < 1
         u = zerosfun(T, size...)
-        u .= rand!(u)
+        rand!(u)
         return rand_gamma(T, 1 + k, θ, fillfun, zerosfun, size...) .* (u .^ (1 / k))
     end
 
@@ -43,17 +44,14 @@ function rand_gamma(T::Type, k, θ, fillfun, zerosfun, size...)
         maskv = copy(masku)
         while any(maskv)
             x[maskv] .= randn!(x[maskv])
-            # println("$(x[maskv])")
             v[maskv] .= 1 .+ c .* x[maskv]
             maskv[maskv] .= v[maskv] .<= 0
-            # println("maskv: $maskv")
         end
 
         @. v[masku] = v[masku] * v[masku] * v[masku]
         u[masku] .= rand!(u[masku])
         @. masku[masku] = masku[masku] & !(u[masku] < 1 - 0.0331 * x[masku] * x[masku] * x[masku] * x[masku])
         @. masku[masku] = masku[masku] & !(log(u[masku]) < (0.5 * x[masku] * x[masku] + d * (1 - v[masku] + log(v[masku]))))
-        # println("masku: $masku")
         if !any(masku) 
             break
         end
@@ -61,11 +59,77 @@ function rand_gamma(T::Type, k, θ, fillfun, zerosfun, size...)
     return θ .* d .* v
 end
 
-# rand_gamma_u_sample!(masku, v, u, x, d) = rand_gamma_u_sample!(v[masku], u[masku], x[masku], d)
-# function rand_gamma_u_sample!(v::AbstractArray{T, N}, u::AbstractArray{T, N}, x::AbstractArray{T, N}, d) where {T, N}
-#     @. v = v * v * v
-#     u .= rand!(u)
-#     a = @. !(u < 1 - 0.0331 * x * x * x * x) 
-#     b = @. !(log(u) < (0.5 * x * x + d * (1 - v + log(v))))
-#     a .| b
-# end
+rand_gamma_gpu(k::Number, θ::Number, size::Int...) = rand_gamma_gpu(Float32, k, θ, size...)
+function rand_gamma_gpu(T::Type, k, θ, size...)
+    k = T(k)
+    θ = T(θ)
+    if k < 1
+        u = cuzeros(T, size...)
+        rand!(u)
+        return rand_gamma_gpu(T, 1 + k, θ, size...) .* CUDAnative.pow.(u, (1 / k))
+    end
+
+    x = cuzeros(T, size...)
+    v = cuzeros(T, size...)
+    u = cuzeros(T, size...)
+
+    d = k - 1 / T(3)
+    c = 1 / (3 * sqrt(d))
+
+    masku = cufill(true, size...)
+    while any(masku)
+        maskv = copy(masku)
+        while any(maskv)
+            v .= map((m, v, nv) -> m ? nv : v, maskv, v, next_gamma_v_sample(x, c))
+            maskv .= maskv .& (v .<= 0)
+        end
+        x .= (v .- 1) ./ c
+
+        v .= map((m, v, nv) -> m ? nv : v, masku, v, v .* v .*v)
+        rand!(u)
+        @. masku = masku & !(u < 1 - 0.0331 * x * x * x * x)
+        @. masku = masku & !(CUDAnative.log(u) < (0.5 * x * x + d * (1 - v + CUDAnative.log(v))))
+    end
+    return θ .* d .* v
+end
+
+function next_gamma_v_sample(x, c)
+    randn!(x)
+    return 1 .+ c .* x
+end
+
+rand_beta_gpu(α::Number, β::Number, size::Int...) = rand_beta_gpu(Float32, α, β, size...)
+function rand_beta_gpu(T::Type, α, β, size...)
+    α = T(α)
+    β = T(β)
+
+    if (α > 1) || (β > 1)
+        g1 = rand_gamma_gpu(T, α, 1, size...)
+        g2 = rand_gamma_gpu(T, β, 1, size...)
+        return g1 ./ (g1 .+ g2)
+    end
+
+    u = cuzeros(T, size...)
+    v = cuzeros(T, size...)
+    x = cuzeros(T, size...)
+    y = cuzeros(T, size...)
+
+    mask = cufill(true, size...)
+    while any(mask)
+        rand!(u)
+        rand!(v)
+        x .= map((m, x, nx) -> m ? nx : x, mask, x, CUDAnative.pow.(u, (1 / α)))
+        y .= map((m, y, ny) -> m ? ny : y, mask, y, CUDAnative.pow.(v, (1 / β)))
+        @. mask = mask & ((x + y) > 1)
+    end
+
+    return map((x, y) -> (x + y) > 0 ? x / (x + y) : log_beta_expression(CUDAnative.log(x), CUDAnative.log(y)), x, y)
+end
+
+function log_beta_expression(logX, logY)
+    logM = logX > logY ? logX : logY
+    logX -= logM
+    logY -= logM
+    return CUDAnative.exp(logX - CUDAnative.log(CUDAnative.exp(logX) + CUDAnative.exp(logY)))
+end
+
