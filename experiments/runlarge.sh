ff650d2dad3cbf9a05eab68a78dde2ec1bd52176
Jan Bim <bimjan@bayes.felk.cvut.cz>
2019-05-16 07:58:37 +0200

    checkpoint

--- experiments/runlarge.sh
+++ experiments/runlarge.sh
@@ -1,17 +1,17 @@
-julia twostage_vae.jl abalone &
-sleep 40
+# julia twostage_vae.jl abalone &
+# sleep 40
 # julia twostage_vae.jl blood-transfusion &
 # sleep 40
-julia twostage_vae.jl breast-tissue &
-sleep 40
-julia twostage_vae.jl haberman &
-sleep 40
+# julia twostage_vae.jl breast-tissue &
+# sleep 40
+# julia twostage_vae.jl haberman &
+# sleep 40
 # julia twostage_vae.jl iris &
 # sleep 40
 julia twostage_vae.jl breast-cancer-wisconsin &
 sleep 40
-julia twostage_vae.jl cardiotocography &
-sleep 40
+# julia twostage_vae.jl cardiotocography &
+# sleep 40
 # julia twostage_vae.jl ecoli &
 # sleep 40
 # julia twostage_vae.jl glass &
@@ -22,12 +22,12 @@ sleep 40
 # sleep 40
 # julia twostage_vae.jl statlog-satimage &
 # sleep 40
-julia twostage_vae.jl waveform-1 &
-sleep 40
+# julia twostage_vae.jl waveform-1 &
+# sleep 40
 # julia twostage_vae.jl waveform-2 &
 # sleep 40
 julia twostage_vae.jl yeast &
 sleep 40
-julia twostage_vae.jl ionosphere &
-sleep 40
+# julia twostage_vae.jl ionosphere &
+# sleep 40
 julia twostage_vae.jl wine &
