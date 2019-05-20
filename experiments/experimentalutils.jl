074543ffaf8f634254cc69b4e1988bfe38eed1bd
honzabim <honzabim@gmail.com>
2019-05-20 16:16:40 +0200

    making it work with docker

--- experiments/experimentalutils.jl
+++ experiments/experimentalutils.jl
@@ -14,13 +14,23 @@ gridsearch(f, parameters...) = map(p -> f(p), Base.product(parameters...))
 
 global mainfolder = "D:/dev/julia/"
 if !isdir(mainfolder)
-    mainfolder = "/home/bimjan/dev/julia/"
-    if !isdir(mainfolder)
-        error("The main folder is unknown")
-    end
+    mainfolder = "/home/bimjan/dev/julia/" 
+end
+if !isdir(mainfolder)
+    mainfolder = "/app/" 
+end
+if !isdir(mainfolder)
+    error("The main folder is unknown")
 end
 const datafolder = mainfolder * "data/loda/public/datasets/numerical"
+const server_main_folder = "/home/bimjan/dev/julia/"
 
 loaddata(dataset, difficulty) =  ADatasets.makeset(ADatasets.loaddataset(dataset, difficulty, datafolder)..., 0.8, "low")
 
 computeauc(score, labels) = EvalCurves.auc(EvalCurves.roccurve(score, labels)...)
+
+copytoserver(from, to) = run(`scp $from bayes:$to`)
+function create_server_folder(folder) 
+    inner_cmd = "mkdir -p $folder"
+    run(`ssh bayes $inner_cmd`)
+end
